83936143d59ce520fca78aed4aa6e9602ca74906
Joram Schrijver <i@joram.io>
2016-11-26 11:06:52 +0100

    Fix SPI initialization

--- src/pleasant-spi.c
+++ src/pleasant-spi.c
@@ -8,10 +8,10 @@ bool spi_prepared = false;
 bool spi_configured = false;
 
 void spi_prepare() {
-  DDRB |= (1 << PORTB5);        /* SCK, Serial ClocK */
-  DDRB ^= ~(1 << PORTB4);       /* MISO: Master In Slave Out */
-  DDRB |= (1 << PORTB3);        /* MOSI: Master Out Slave In */
-  DDRB |= (1 << PORTB2);        /* SS: Slave Select */
+  DDRB |= (1 << PORTB2);            /* SS: Slave Select */
+  SPCR |= (1 << MSTR) | (1 << SPE); /* Enable SPI in master mode */
+  DDRB |= (1 << PORTB5);            /* SCK, Serial ClocK */
+  DDRB |= (1 << PORTB3);            /* MOSI: Master Out Slave In */
 
   spi_prepared = true;
 }
@@ -20,17 +20,14 @@ void spi_configure(enum spi_clock_speed clock_speed,
                    enum spi_bit_order bit_order) {
   if (!spi_prepared) spi_prepare();
 
-  SPCR |= (1 << MSTR) | (1 << SPE);
-  SPCR |= (1 << CPOL) | (1 << CPHA);
-
-  SPSR ^= ~SPI_CLOCK_SPEED_SPSR_MASK;
+  SPSR &= ~SPI_CLOCK_SPEED_SPSR_MASK;
   SPSR |= (clock_speed & (1 << 2)) ? (1 << SPI2X) : 0;
 
-  SPCR ^= ~SPI_CLOCK_SPEED_SPCR_MASK;
+  SPCR &= ~SPI_CLOCK_SPEED_SPCR_MASK;
   SPCR |= (clock_speed & (1 << 1)) ? (1 << SPR1) : 0;
   SPCR |= (clock_speed & (1 << 0)) ? (1 << SPR0) : 0;
 
-  SPCR ^= ~SPI_BIT_ORDER_MASK;
+  SPCR &= ~SPI_BIT_ORDER_MASK;
   SPCR |= bit_order;
 
   spi_configured = true;
